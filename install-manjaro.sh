f8f6b577e353b075dd1146d66802bfc36a2fb231
Forest Anderson <forestkzanderson@gmail.com>
2020-06-11 16:13:05 -0400

    Ubuntu libs

--- install-manjaro.sh
+++ install-manjaro.sh
@@ -4,3 +4,5 @@
 sudo pacman -Syyu
 sudo pamac install visual-studio-code-bin synergy git tmux zsh firefox discord docker docker-compose vlc steam -y
 sudo pamac build spotify --no-confirm -y
+
+source install-linux.sh
--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -7,11 +7,18 @@ sudo apt-get install -y \
     build-essential \
     curl \
     git \
+    libasound2-dev \
+    libatk1.0-dev \
     libbz2-dev \
+    libcairo2-dev \
     libffi-dev \
+    libgdk-pixbuf2.0-dev \
+    libglib2.0-dev \
+    libgtk-3-dev \
     liblzma-dev \
     libncurses5-dev \
     libncursesw5-dev \
+    libpango1.0-dev \
     libreadline-dev \
     libsqlite3-dev \
     libssl-dev \
@@ -26,3 +33,5 @@ sudo apt-get install -y \
     zsh
 sudo snap install discord docker vlc spotify 
 sudo snap install code --classic
+
+source install-linux.sh
