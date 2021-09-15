0283936db35771fabfe507d521538cb861bd0815
Forest Anderson <forestkzanderson@gmail.com>
2021-09-15 14:46:10 -0400

    Added brew installs

---
+++ install-mac.sh
@@ -0,0 +1,27 @@
+arch -arm64 brew install \
+    ansible \
+    bandwhich \
+    consul \
+    drill \
+    dust \
+    exa \
+    fd \
+    fish \
+    gh \
+    htop \
+    hyperfine \
+    neofetch \
+    nomad \
+    packer \
+    poetry \
+    procs \
+    ripgrep \
+    rust \
+    starship \
+    tealdeer \
+    terraform \
+    tokei \
+    topgrade \
+    uutils-coreutils \
+    waypoint \
+    zola
